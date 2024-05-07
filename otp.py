c9de2984fdcf65cd2de1bc442ecd771d564d6fe8
Badal3000 <120331977+Badal3000@users.noreply.github.com>
2024-05-07 21:18:04 +0000

    import os import math import random import smtplib digits="0123456789" OTP="" for i in range(6):     OTP+=digits[math.floor(random.random()*10)] otp = OTP + " is your OTP" msg= otp s = smtplib.SMTP('smtp.gmail.com', 587) s.starttls() s.login("Your Gmail Account", "You app password") emailid = input("Enter your email: ") s.sendmail('&&&&&&&&&&&',emailid,msg) a = input("Enter Your OTP >>: ") if a == OTP:     print("Verified") else:     print("Please Check your OTP again")
    Enter your email: aman@thecleverprogrammer.com
    Enter Your OTP >>: 478348
    Verified
    Once you run this code you enter an email where you want to send an OTP and then enter the OTP that you have received in the email. You can get the complete code used in this article for the task of OTP verification from below.
    import os
    import math
    import random
    import smtplib
    
    digits="0123456789"
    OTP=""
    for i in range(6):
        OTP+=digits[math.floor(random.random()*10)]
    otp = OTP + " is your OTP"
    msg= otp
    s = smtplib.SMTP('smtp.gmail.com', 587)
    s.starttls()
    s.login("Your Gmail Account", "You app password")
    emailid = input("Enter your email: ")
    s.sendmail('&&&&&&&&&&&',emailid,msg)
    a = input("Enter Your OTP >>: ")
    if a == OTP:
        print("Verified")
    else:
        print("Please Check your OTP again")

---
+++ otp.py
@@ -0,0 +1,46 @@
+import os
+import math
+import random
+import smtplib
+digits="0123456789"
+OTP=""
+for i in range(6):
+    OTP+=digits[math.floor(random.random()*10)]
+otp = OTP + " is your OTP"
+msg= otp
+s = smtplib.SMTP('smtp.gmail.com', 587)
+s.starttls()
+s.login("Your Gmail Account", "You app password")
+emailid = input("Enter your email: ")
+s.sendmail('&&&&&&&&&&&',emailid,msg)
+a = input("Enter Your OTP >>: ")
+if a == OTP:
+    print("Verified")
+else:
+    print("Please Check your OTP again")
+
+Enter your email: aman@thecleverprogrammer.com
+Enter Your OTP >>: 478348
+Verified
+Once you run this code you enter an email where you want to send an OTP and then enter the OTP that you have received in the email. You can get the complete code used in this article for the task of OTP verification from below.
+import os
+import math
+import random
+import smtplib
+
+digits="0123456789"
+OTP=""
+for i in range(6):
+    OTP+=digits[math.floor(random.random()*10)]
+otp = OTP + " is your OTP"
+msg= otp
+s = smtplib.SMTP('smtp.gmail.com', 587)
+s.starttls()
+s.login("Your Gmail Account", "You app password")
+emailid = input("Enter your email: ")
+s.sendmail('&&&&&&&&&&&',emailid,msg)
+a = input("Enter Your OTP >>: ")
+if a == OTP:
+    print("Verified")
+else:
+    print("Please Check your OTP again")
